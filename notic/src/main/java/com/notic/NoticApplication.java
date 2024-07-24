7dd627ebff309cd0f645caa317a0f69b241420c9
Breynner Ariza <breynner.fabian.ariza.florez@gmail.com>
2024-07-23 21:05:52 -0500

    Firts commit.

---
+++ notic/src/main/java/com/notic/NoticApplication.java
@@ -0,0 +1,13 @@
+package com.notic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticApplication.class, args);
+	}
+
+}
---
+++ notic/src/test/java/com/notic/NoticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
