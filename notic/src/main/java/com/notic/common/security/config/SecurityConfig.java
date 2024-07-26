2dccfda934f54a9b32d592130adc138275de1c56
Breynner Ariza <breynner.fabian.ariza.florez@gmail.com>
2024-07-25 23:00:51 -0500

    [SecurityConfig] Add components for authentcation Descriptiop     - Add UserDetailService     - Add AuthenticationProvider     - Add AuthenticationManager

--- notic/src/main/java/com/notic/common/security/config/SecurityConfig.java
+++ notic/src/main/java/com/notic/common/security/config/SecurityConfig.java
@@ -1,14 +1,44 @@
 package com.notic.common.security.config;
 
+import com.notic.user.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    AuthenticationManager getAuthenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    AuthenticationProvider getAuthenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(getUserDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(getPasswordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    UserDetailsService getUserDetailsService(){
+        return email -> userRepository.findUserByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Credentials invalid"));
+    }
 
     @Bean
     PasswordEncoder getPasswordEncoder(){
--- notic/src/main/java/com/notic/user/domain/User.java
+++ notic/src/main/java/com/notic/user/domain/User.java
@@ -4,7 +4,10 @@ import com.notic.taskgroup.domain.TaskGroup;
 import com.notic.accesstoken.domain.AccessToken;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.Set;
 
 @Entity
@@ -13,7 +16,7 @@ import java.util.Set;
 @Getter @Setter
 @EqualsAndHashCode(exclude = {"accessTokens", "taskGroups"})
 @ToString(exclude = {"accessTokens", "taskGroups"})
-public class User {
+public class User implements UserDetails {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
@@ -29,5 +32,16 @@ public class User {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST, fetch =FetchType.LAZY)
     private Set<TaskGroup> taskGroups;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
 
 }
